d066cd52113da19da514dd5879e0ff3fd047b72f
Badder <derbassi@gmail.com>
2022-02-27 23:02:46 +0100

    Push all scripts

---
+++ checkprocesses/checkprocesses.ps1
@@ -0,0 +1,9 @@
+﻿$Processes = Get-Process
+$RequiredProcesses = "Chrome", "Outloock", "Firefox"
+foreach($p in $RequiredProcesses){
+if($processes.ProcessName -notcontains $p){
+    Write-Host "$p not running " -BackgroundColor DarkGreen -ForegroundColor Yellow
+    
+ }else{Write-Host "$p is already running." -BackgroundColor DarkCyan -ForegroundColor White
+}
+}
